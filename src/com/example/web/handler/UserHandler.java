05282aa80976ab420201c73513674d4568f86486
lghuntfor <lghuntfor@sina.com>
2016-03-06 00:09:20 +0800

    up

--- src/com/example/web/handler/UserHandler.java
+++ src/com/example/web/handler/UserHandler.java
@@ -1,9 +1,7 @@
 package com.example.web.handler;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.entity.UploadImg;
