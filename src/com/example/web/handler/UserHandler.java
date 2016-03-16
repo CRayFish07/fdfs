2653e700285aa0a06f431728f70dab52ce6f5cba
lghuntfor <761193295@qq.com>
2016-03-16 15:22:00 +0800

    去除本地下载

--- src/com/example/web/handler/UserHandler.java
+++ src/com/example/web/handler/UserHandler.java
@@ -64,18 +64,11 @@ public class UserHandler {
 	@RequestMapping(value="/upload", method=RequestMethod.POST)
 	public String upload(@RequestParam MultipartFile img) {
 		String filename = img.getOriginalFilename();
-		System.out.println(filename);
 		String extName = filename.substring(filename.lastIndexOf("."));
 		filename = Encodes.encodeByMD5(filename+System.currentTimeMillis())+extName;
-		System.out.println(filename);
-		try {
-			FileUtil.uploadFileToServer(img.getInputStream(), Const.UPLOAD_TEMP_DIR, filename);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 		String path = null;
 		try {
-			path = DfsUtils.upload(Const.UPLOAD_TEMP_DIR+filename);
+			path = DfsUtils.upload(img.getInputStream(), extName);
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (MyException e) {
